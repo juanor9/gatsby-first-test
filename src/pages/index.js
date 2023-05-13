db01847cbb124aa2efe00465047b3d1b89185bec
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-05-13 10:07:02 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
     <p>I'm making this by following the Gatsby Tutorial.</p>
+    <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/Nishinegi_series_tracking_website_ui_ux_uiux_minimalistic_moder_41a64654-ab96-466a-ba95-bc69d08b0973.png"
+      />
   </Layout>
   )
 }
